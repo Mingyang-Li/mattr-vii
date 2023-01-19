af1cae71cf556763397d268e1666d5306c988a20
Mingyang-Li-MATTR <mingyang.li@mattr.global>
2023-01-19 17:35:52 +1300

    feat: bundled CredentialService

--- src/services/web-semantic-credentials/credential.service.ts
+++ src/services/web-semantic-credentials/credential.service.ts
@@ -4,8 +4,28 @@ const createCredential = (auth: IAuth) => async () => {
   return { auth };
 };
 
-export const LinkedDataService = (auth: IAuth) => {
+const retrieveCredentials = (auth: IAuth) => async () => {
+  return { auth };
+};
+
+const retrieveCredential = (auth: IAuth) => async () => {
+  return { auth };
+};
+
+const removeCredential = (auth: IAuth) => async () => {
+  return { auth };
+};
+
+const verifyCredential = (auth: IAuth) => async () => {
+  return { auth };
+};
+
+export const CredentialService = (auth: IAuth) => {
   return {
     createCredential: createCredential(auth),
+    retrieveCredentials: retrieveCredentials(auth),
+    retrieveCredential: retrieveCredential(auth),
+    removeCredential: removeCredential(auth),
+    verifyCredential: verifyCredential(auth),
   };
 };
